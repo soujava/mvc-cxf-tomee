b62d0a4ffc42e540ec9dbb1dd1ad2a9ecd6a7959
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-01 13:35:59 -0200

    uses configuration

--- src/main/java/org/superbiz/persistence/PersonProducer.java
+++ src/main/java/org/superbiz/persistence/PersonProducer.java
@@ -16,27 +16,28 @@
  */
 package org.superbiz.persistence;
 
-import org.jnosql.diana.api.Settings;
+import org.jnosql.artemis.ConfigurationUnit;
 import org.jnosql.diana.api.document.DocumentCollectionManager;
 import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
-import org.jnosql.diana.api.document.DocumentConfiguration;
-import org.jnosql.diana.mongodb.document.MongoDBDocumentConfiguration;
+import org.jnosql.diana.mongodb.document.MongoDBDocumentCollectionManager;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class PersonProducer {
 
     private static final String DOCUMENT_COLLECTION = "people";
 
+    @Inject
+    @ConfigurationUnit
+    private DocumentCollectionManagerFactory<MongoDBDocumentCollectionManager> managerFactory;
+
     @Produces
     @ApplicationScoped
     public DocumentCollectionManager getDocumentCollectionManager() {
-        DocumentConfiguration<?> configuration = new MongoDBDocumentConfiguration();
-        Settings settings = Settings.builder().put("mongodb-server-host-1", "localhost:27017").build();
-        DocumentCollectionManagerFactory managerFactory = configuration.get(settings);
         return managerFactory.get(DOCUMENT_COLLECTION);
     }
 
